42baed2666788e4d948d20e040693a86368fb6c1
0x-m1cro <0xm1cro@proton.me>
2023-09-04 17:46:47 +0500

    working#11

--- api/index.js
+++ api/index.js
@@ -1,15 +1,12 @@
-const chromium = require('playwright')
+const playwright = require("playwright")
 
 
 module.exports = async (req, res) => {
-
   let { hotelid, checkin, checkout  } = req.query
-
-  for (const browserType of ['chromium', 'firefox',  'webkit']){
-   const launchOptions = {
+  const launchOptions = {
        headless: false,
      }
-   const browser = await chromium.launch()
+   const browser = await playwright["chromium"].launch(launchOptions)
    const context = await browser.newContext()
    const page = await context.newPage()
    await page.goto(`https://hotelscan.com/combiner/${hotelid}?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=2&mobile=0&loop=1&country=MV&ef=1&geoid=xmmmamtksdxx&toas=resort&availability=1&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0`);
@@ -19,7 +16,6 @@ module.exports = async (req, res) => {
    let res = await body?.evaluate(el => el.textContent);
    console.log(res)
    await browser.close()
-   }
 
   res.status(200).json(res)
 }
