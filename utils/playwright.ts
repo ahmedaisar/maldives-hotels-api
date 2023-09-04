11fbe096bbf80dc965d68af73e33d24c1c791f21
0x-m1cro <0xm1cro@proton.me>
2023-09-04 11:40:07 +0500

    init#2

--- utils/playwright.ts
+++ utils/playwright.ts
@@ -12,28 +12,32 @@ export default (params: Params) => {
   return new Promise(async (resolve, reject) => {
     let { arrival, departure, hotelid } = params
 
-    const browser = await playwright.launchChromium({ headless: true })
-    const context = await browser.newContext({ screen: { width: 4096, height: 4096 } })
-    const page = await context.newPage()
+    console.log(params)
 
-    await page.goto(`https://hotelscan.com/combiner/${hotelid}?pos=zz&locale=en&checkin=${arrival}&checkout=${departure}&rooms=2&mobile=0&loop=1&country=MV&ef=1&geoid=xmmmamtksdxx&toas=resort&availability=1&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0`)
+    resolve(params)
+
+    // const browser = await playwright.launchChromium({ headless: true })
+    // const context = await browser.newContext({ screen: { width: 4096, height: 4096 } })
+    // const page = await context.newPage()
+
+    // await page.goto(`https://hotelscan.com/combiner/${hotelid}?pos=zz&locale=en&checkin=${arrival}&checkout=${departure}&rooms=2&mobile=0&loop=1&country=MV&ef=1&geoid=xmmmamtksdxx&toas=resort&availability=1&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0`)
 
 
-    let body = await page.waitForSelector('body');
+    // let body = await page.waitForSelector('body');
 
-    let res = await body?.evaluate(el => el.textContent);
+    // let res = await body?.evaluate(el => el.textContent);
     
-    // let html = await page.evaluate(() => {
-    // let body = document.querySelector("body").innerText;
-    //   //let pre = document.querySelector("pre").innerHTML;
-    //   return JSON.parse(body);
-    // });
-    const data = JSON.stringify(res);
+    // // let html = await page.evaluate(() => {
+    // // let body = document.querySelector("body").innerText;
+    // //   //let pre = document.querySelector("pre").innerHTML;
+    // //   return JSON.parse(body);
+    // // });
+    // const data = JSON.stringify(res);
 
-    await browser.close()
+    // await browser.close()
 
-    console.log(data);
+    // console.log(data);
 
-    resolve(data)
+    // resolve(data)
   })
 }
