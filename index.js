4db931a29ebacaa4fe335084f4803f7c015df4de
0x-m1cro <0xm1cro@proton.me>
2023-09-05 17:32:33 +0500

    new build with express

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+let chrome = {};
+let puppeteer;
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  chrome = require("chrome-aws-lambda");
+  puppeteer = require("puppeteer-core");
+} else puppeteer = require("puppeteer");
+
+
+app.use(express.json());
+app.use(cors());
+
+async function getHotel(hotelid, checkin, checkout) {
+
+  let options = { headless: 'new' };
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    options = {
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: 'new',
+      ignoreHTTPSErrors: true,
+    };
+  }
+  const browser = await puppeteer.launch(options);
+  const page = await browser.newPage();
+  let data = []
+
+  try {
+    await page.goto(`https://hotelscan.com/combiner/${hotelid}?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=2&mobile=0&loop=1&country=MV&ef=1&geoid=xmmmamtksdxx&toas=resort&availability=1&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0`);
+    await page.waitForTimeout(1000);
+    let body = await page.waitForSelector('body');
+    let json = await body?.evaluate(el => el.textContent);
+    data.push(json);
+    return data;
+  } catch (error) {
+    console.error('Error:', error);
+    await browser.close();
+    throw error;
+  }
+}
+
+app.get("/api/maldives/hotel", async function (req, res) {
+  const { hotelid, checkin, checkout } = req.query
+
+  const data = await getHotel(hotelid, checkin, checkout);
+
+  return res.json(data);
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT} `);
+})
+
+module.exports = app;
+
+
