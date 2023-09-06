29ab1e68d31f670cc689c73fd30a8e67563bc869
0x-m1cro <0xm1cro@proton.me>
2023-09-06 15:27:40 +0500

    netlify-init

--- index.js
+++ index.js
@@ -1,32 +1,56 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-let chrome = require("chrome-aws-lambda");
-let puppeteer = require("puppeteer-core");
+// const express = require('express');
+// const app = express();
+// const cors = require('cors');
+// let chrome = require("chrome-aws-lambda");
+// let puppeteer = require("puppeteer-core");
 
-// let chrome = {};
-// let puppeteer;
+// // let chrome = {};
+// // let puppeteer;
 
-// if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-//   let chrome = require("chrome-aws-lambda");
-//   let puppeteer = require("puppeteer-core");
-// } 
+// // if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+// //   let chrome = require("chrome-aws-lambda");
+// //   let puppeteer = require("puppeteer-core");
+// // } 
 
 
-app.use(express.json());
-app.use(cors());
+// app.use(express.json());
+// app.use(cors());
 
-// async function getHotel(hotelid, checkin, checkout) {
+// // async function getHotel(hotelid, checkin, checkout) {
 
-//   let options = { headless: 'new' };
-//   if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-//     options = {
-//       executablePath: await chrome.executablePath,
-//       headless: 'new',
-//     };
-//   }
+// //   let options = { headless: 'new' };
+// //   if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+// //     options = {
+// //       executablePath: await chrome.executablePath,
+// //       headless: 'new',
+// //     };
+// //   }
+// //   const browser = await puppeteer.launch(options);
+// //   const page = await browser.newPage();
+// //   let data = []
+
+// //   try {
+// //     await page.goto(`https://hotelscan.com/combiner/${hotelid}?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=2&mobile=0&loop=1&country=MV&ef=1&geoid=xmmmamtksdxx&toas=resort&availability=1&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0`);
+// //     await page.waitForTimeout(1000);
+// //     let body = await page.waitForSelector('body');
+// //     let json = await body?.evaluate(el => el.textContent);
+// //     data.push(json);
+// //     return data;
+// //   } catch (error) {
+// //     console.error('Error:', error);
+// //     await browser.close();
+// //     throw error;
+// //   }
+// // }
+
+// app.get("/api/maldives/hotel", async function (req, res) {
+//   const { hotelid, checkin, checkout } = req.query
+
+//   let options = { executablePath: await chrome.executablePath,
+//   headless: 'new', };
 //   const browser = await puppeteer.launch(options);
 //   const page = await browser.newPage();
+//   console.log(hotelid, checkin, checkout);
 //   let data = []
 
 //   try {
@@ -35,44 +59,20 @@ app.use(cors());
 //     let body = await page.waitForSelector('body');
 //     let json = await body?.evaluate(el => el.textContent);
 //     data.push(json);
-//     return data;
+//     await browser.close();
 //   } catch (error) {
 //     console.error('Error:', error);
 //     await browser.close();
 //     throw error;
 //   }
-// }
 
-app.get("/api/maldives/hotel", async function (req, res) {
-  const { hotelid, checkin, checkout } = req.query
+//   return res.json(data);
+// })
 
-  let options = { executablePath: await chrome.executablePath,
-  headless: 'new', };
-  const browser = await puppeteer.launch(options);
-  const page = await browser.newPage();
-  console.log(hotelid, checkin, checkout);
-  let data = []
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on ${PORT} `);
+// })
 
-  try {
-    await page.goto(`https://hotelscan.com/combiner/${hotelid}?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=2&mobile=0&loop=1&country=MV&ef=1&geoid=xmmmamtksdxx&toas=resort&availability=1&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0`);
-    await page.waitForTimeout(1000);
-    let body = await page.waitForSelector('body');
-    let json = await body?.evaluate(el => el.textContent);
-    data.push(json);
-    await browser.close();
-  } catch (error) {
-    console.error('Error:', error);
-    await browser.close();
-    throw error;
-  }
-
-  return res.json(data);
-})
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT} `);
-})
-
-module.exports = app;
+// module.exports = app;
 
---
+++ netlify/functions/api.js
@@ -0,0 +1,38 @@
+import express, { Router } from 'express';
+import serverless from 'serverless-http';
+const cors = require('cors');
+let chrome = require("chrome-aws-lambda");
+let puppeteer = require("puppeteer-core");
+
+const api = express();
+
+const router = Router();
+router.get('/hello', (req, res) => res.send('Hello World!'));
+router.get("/maldives/hotel", async function (req, res) {
+    const { hotelid, checkin, checkout } = req.query
+    let options = { executablePath: await chrome.executablePath,
+    headless: 'new', };
+    const browser = await puppeteer.launch(options);
+    const page = await browser.newPage();
+    console.log(hotelid, checkin, checkout);
+    let data = []
+  
+    try {
+      await page.goto(`https://hotelscan.com/combiner/${hotelid}?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=2&mobile=0&loop=1&country=MV&ef=1&geoid=xmmmamtksdxx&toas=resort&availability=1&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0`);
+      await page.waitForTimeout(1000);
+      let body = await page.waitForSelector('body');
+      let json = await body?.evaluate(el => el.textContent);
+      data.push(json);
+      await browser.close();
+    } catch (error) {
+      console.error('Error:', error);
+      await browser.close();
+      throw error;
+    }
+  
+    return res.json(data);
+  })
+
+api.use('/api/', router);
+
+export const handler = serverless(api);
