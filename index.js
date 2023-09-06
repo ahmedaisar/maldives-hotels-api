ccae0d0d61026752a82f64385ca8bbbda80733c7
0x-m1cro <0xm1cro@proton.me>
2023-09-06 15:05:51 +0500

    express#6

--- index.js
+++ index.js
@@ -16,17 +16,41 @@ let puppeteer = require("puppeteer-core");
 app.use(express.json());
 app.use(cors());
 
-async function getHotel(hotelid, checkin, checkout) {
+// async function getHotel(hotelid, checkin, checkout) {
 
-  let options = { headless: 'new' };
-  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    options = {
-      executablePath: await chrome.executablePath,
-      headless: 'new',
-    };
-  }
+//   let options = { headless: 'new' };
+//   if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+//     options = {
+//       executablePath: await chrome.executablePath,
+//       headless: 'new',
+//     };
+//   }
+//   const browser = await puppeteer.launch(options);
+//   const page = await browser.newPage();
+//   let data = []
+
+//   try {
+//     await page.goto(`https://hotelscan.com/combiner/${hotelid}?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=2&mobile=0&loop=1&country=MV&ef=1&geoid=xmmmamtksdxx&toas=resort&availability=1&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0`);
+//     await page.waitForTimeout(1000);
+//     let body = await page.waitForSelector('body');
+//     let json = await body?.evaluate(el => el.textContent);
+//     data.push(json);
+//     return data;
+//   } catch (error) {
+//     console.error('Error:', error);
+//     await browser.close();
+//     throw error;
+//   }
+// }
+
+app.get("/api/maldives/hotel", async function (req, res) {
+  const { hotelid, checkin, checkout } = req.query
+
+  let options = { headless: 'new', executablePath: await chrome.executablePath,
+  headless: 'new', };
   const browser = await puppeteer.launch(options);
   const page = await browser.newPage();
+  console.log(hotelid, checkin, checkout);
   let data = []
 
   try {
@@ -35,18 +59,12 @@ async function getHotel(hotelid, checkin, checkout) {
     let body = await page.waitForSelector('body');
     let json = await body?.evaluate(el => el.textContent);
     data.push(json);
-    return data;
+    await browser.close();
   } catch (error) {
     console.error('Error:', error);
     await browser.close();
     throw error;
   }
-}
-
-app.get("/api/maldives/hotel", async function (req, res) {
-  const { hotelid, checkin, checkout } = req.query
-
-  const data = await getHotel(hotelid, checkin, checkout);
 
   return res.json(data);
 })
