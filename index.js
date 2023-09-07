626b58894e285d6e5d6ce8bd8963f11c6b329c8c
0x-m1cro <0xm1cro@proton.me>
2023-09-07 16:51:13 +0500

    render#3

--- index.js
+++ index.js
@@ -3,36 +3,56 @@ const app = express();
 const cors = require('cors');
 const chrome = require("chrome-aws-lambda");
 
-let puppeteer;
-
 app.use(express.json());
 app.use(cors());
 
 async function getHotel(hotelid, checkin, checkout) {
 
   const options = {
-    args: chrome.args,
+    args: await chrome.args,
     executablePath: await chrome.executablePath,
-    headless: chrome.headless,
+    headless: await chrome.headless,
   };
-  const browser = await chrome.puppeteer.launch(options);
-  const page = await browser.newPage();
-  let data = []
+  // const browser = await chrome.puppeteer.launch(options);
+  // const page = await browser.newPage();
+  // let data = []
 
   try {
-    await page.goto(`https://hotelscan.com/combiner/${hotelid}?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=2&mobile=0&loop=1&country=MV&ef=1&geoid=xmmmamtksdxx&toas=resort&availability=1&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0`, {
-      waitUntil: "networkidle2",
+    const browser = await chrome.puppeteer.launch(options);
+
+    const page = await browser.newPage();
+
+    await page.goto(
+      `https://hotelscan.com/combiner/${hotelid}?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=${rooms}&mobile=0&loop=1&country=MV&ef=1&geoid=xmmmamtksdxx&toas=resort&availability=1&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0`,
+      {
+        waitUntil: "networkidle2",
+      }
+    );
+    //await page.waitForTimeout(2000);
+    let html = await page.evaluate(() => {
+      return JSON.parse(document.querySelector("body").innerText);
     });
-    let body = await page.waitForSelector('body');
-    let json = await body?.evaluate(el => el.textContent);
-    data.push(json);
-    return data;
+    await browser.close();    
+    console.log(html);
+    return html;
   } catch (error) {
-    console.error('Error:', error);
-    await browser.close();
-    throw error;
+    console.log(error);
   }
 }
+
+  // try {
+  //   await page.goto(`https://hotelscan.com/combiner/${hotelid}?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=2&mobile=0&loop=1&country=MV&ef=1&geoid=xmmmamtksdxx&toas=resort&availability=1&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0`, {
+  //     waitUntil: "networkidle2",
+  //   });
+  //   let body = await page.waitForSelector('body');
+  //   let json = await body?.evaluate(el => el.textContent);
+  //   data.push(json);
+  //   return data;
+  // } catch (error) {
+  //   console.error('Error:', error);
+  //   await browser.close();
+  //   throw error;
+  // }
 
 app.get("/api/maldives/hotel", async function (req, res) {
   const { hotelid, checkin, checkout } = req.query
