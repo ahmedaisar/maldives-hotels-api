3d941382e455f298b5cd458acad3283c363ff328
0x-m1cro <0xm1cro@proton.me>
2023-09-07 15:38:36 +0500

    netlifydev#7

--- api.js
+++ api.js
@@ -15,7 +15,7 @@ router.get("/maldives/hotel", function (req, res) {
     const { hotelid, checkin, checkout } = req.query
     let data = []
     const config = {
-      method: 'get',
+      method: 'GET',
       url: `https://hotelscan.com/combiner/${hotelid}?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=2&mobile=0&loop=1&country=MV&ef=1&geoid=xmmmamtksdxx&toas=resort&availability=1&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0`,
     };    
     axios(config).then(response => { data.push(response.data)})
