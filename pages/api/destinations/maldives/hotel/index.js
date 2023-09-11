bceb0a399461719a718623240e8bc4b68f920055
0x-m1cro <0xm1cro@proton.me>
2023-09-11 11:34:43 +0500

    render#nextjs

---
+++ pages/api/destinations/maldives/hotel/index.js
@@ -0,0 +1,36 @@
+const chrome = require("chrome-aws-lambda");
+
+export default async function handler(req, res) {
+  let query = req.query;
+  const { hotelid, checkin, checkout, rooms } = query;
+
+  const options = {
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless,
+  };
+
+  try {
+    const browser = await chrome.puppeteer.launch(options);
+
+    const page = await browser.newPage();
+
+    await page.goto(
+      `https://hotelscan.com/combiner/${hotelid}?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=${rooms}&mobile=0&loop=1&country=MV&ef=1&geoid=xmmmamtksdxx&toas=resort&availability=1&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0`,
+      {
+        waitUntil: "networkidle2",
+      }
+    );
+    let body = await page.waitForSelector('body');
+    let json = await body?.evaluate(el => el.textContent);
+    //await page.waitForTimeout(2000);
+    // let html = await page.evaluate(() => {
+    //   return JSON.parse(document.querySelector("body").innerText);
+    // });
+    await browser.close();
+    res.status(200).json(json);
+    console.log(html);
+  } catch (error) {
+    console.log(error);
+  }
+}
